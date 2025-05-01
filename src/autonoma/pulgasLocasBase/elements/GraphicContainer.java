737a0e78ecbf612d2eb21b92fc4af1b9a2353615
marianaRivas02 <rivasclavijom12@gmail.com>
2025-05-01 10:53:13 -0500

    creacion de algunas clases de sprite

---
+++ src/autonoma/pulgasLocasBase/elements/GraphicContainer.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.pulgasLocasBase.elements;
+
+/**
+ *
+ * @author maria
+ */
+public interface GraphicContainer {
+    
+}
